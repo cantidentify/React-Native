8fefb0519c29e1f74fe1fac8c72d9f828df27792
Phongpisut Meemuk <me.phongpisut_st@tni.ac.th>
2021-01-26 17:59:09 +0700

    Camera Testing

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import * as React from 'react';
-import { View, Image, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Animated, Text, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, StyleSheet, TouchableOpacity, Animated, Text, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+
 
 import  Home  from './screen/layout';
 import Info from './screen/info';
+
+let Stack = createStackNavigator()
+
+
 
 function HomeScreen() {
   return (
@@ -15,9 +23,53 @@ function HomeScreen() {
 }
 
 function SettingsScreen() {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  
+useEffect(() => {
+  
+  (async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    setHasPermission(status === 'granted');
+  })();
+}, [])
+
+
+if (hasPermission === null) {
+  return <View />;
+}
+
+if (hasPermission === false) {
+  return <Text>No access to camera</Text>;
+}
+
+
   return (
+
+    
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+          <View>
+      <Camera     style={{flex: 1,width:"100%"}}
+    ref={(r) => {
+    camera = r
+    }}>
+        <View>
+          <TouchableOpacity
+          style={{width:300,backgroundColor:"#ff55"}}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
     </View>
   );
 }
@@ -26,19 +78,34 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
+
+
     <Tab.Navigator>
       <Tab.Screen name="หน้าหลัก" component={Home} />
       <Tab.Screen name="สถานที่" component={SettingsScreen} />
       <Tab.Screen name="ข่าวสาร" component={SettingsScreen} />
       <Tab.Screen name="ข้อมูลวัด" component={Info} />
     </Tab.Navigator>
+
+
+
+
   );
 }
 
 export default function App() {
+
+ 
+
+
+
+
+
   return (
     <NavigationContainer>
       <MyTabs />
     </NavigationContainer>
+
+
   );
 }
